3bcb68400f2d09346c7f21959ef952aeba04856b
Charles <charliecharleston1121@gmail.com>
2023-09-12 21:22:36 +0100

    stage one task

--- Stage one task/api.py
+++ Stage one task/api.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
-from datetime import datetime,timezone
+from flask import Flask, request, jsonify
+from datetime import datetime, timezone
+
 app = Flask(__name__)
 
 @app.route('/api')
@@ -7,17 +8,17 @@ def api():
     slack_name = request.args.get("slack_name",str)
     track = request.args.get("track",str)
     now = datetime.now(timezone.utc)
-    utcnow = datetime.datetime.now(tz=datetime.timezone.utc)
+
     user_data ={
         "slack_name":"charliee",
         "current_day":now.strftime('%A'),
-        "utc_time":utcnow,
+        "utc_time":now.strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track":"backend",
         "github_file_url":"https://github.com/Creature-of-Habit/HNGX/blob/main/Stage%20one%20task/api.py",
         "github_repo_url":"https://github.com/Creature-of-Habit/HNGX.git",
         "status_code": 200
     }
-    
+
     if (slack_name == user_data["slack_name"]) & ( track == user_data["track"]) :
         return jsonify(user_data), 200
 
